10674b069dd322e36344ff335d985d6513616b38
wancheng-salary <wan.qc013@gmail.com>
2026-06-02 21:56:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
 pdfmetrics.registerFont(
-    TTFont("DejaVu", "DejaVuSans.ttf")
+    TTFont("NotoSans", "NotoSansTC-Regular.ttf")
 )
 
 st.title("每月薪資計算系統 / Hệ thống tính lương hàng tháng")
