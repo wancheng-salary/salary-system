a7944c31c2e6d7aef5b49e35fe8084ab84a445cb
wancheng-salary <wan.qc013@gmail.com>
2026-06-04 13:14:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -40,8 +40,9 @@ credentials = Credentials.from_service_account_info(
     scopes=scope
 )
 
-gc = gspread.authorize(credentials)
 sheet = gc.open_by_key(SHEET_ID).worksheet("salary_records")
+gc = gspread.authorize(credentials)
+
 
 pdfmetrics.registerFont(
     TTFont("NotoSans", "NotoSansTC-VariableFont_wght.ttf")
